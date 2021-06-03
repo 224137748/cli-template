e5815e6880cff4990378b7a5d28b2df881f8d2b0
ZhangJunrui <zhangjunrui@ludashi.com>
2021-06-03 15:43:01 +0800

    更新模板

---
+++ src/example/vue/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <hello-world :msg="'你好啊，李银河！'" />
+    <h1>{{ count }}</h1>
+    <button @click="handleClick">按钮</button>
+  </div>
+</template>
+<script>
+import { defineComponent, ref } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+export default defineComponent({
+  name: "app",
+  components: {
+    HelloWorld,
+  },
+  setup: () => {
+    const count = ref(0);
+
+    const handleClick = () => {
+      count.value++;
+      console.log(count.value);
+    };
+
+    return {
+      count,
+      handleClick,
+    };
+  },
+});
+</script>
+<style lang="less">
+* {
+  padding: 0;
+  margin: 0;
+}
+</style>
---
+++ src/example/vue/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  title: 'Vue template',
+  description: '梦想总是遥不可及',
+  keyword: '',
+  script: [],
+  frame: 'vue'
+}
